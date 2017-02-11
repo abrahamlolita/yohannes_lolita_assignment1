3c6b2f00a95d3224aec885565851806d9fc46271
abrahamlolita <abrahamlolita@gmail.com>
2017-02-11 12:01:40 -0800

    COMM 644 - Assignment I
    COMM 644 - Assignment I

---
+++ script.js
@@ -0,0 +1,145 @@
+//Lolita Yohannes
+//COMM 644 - Assignement 1
+
+// STEP 1
+//var someMonth;
+//function theCurrentMonth() {};		
+//const currentMonth;		            
+//var summerMonths;          
+//var funcnMyLibrary = function () {};
+
+// STEP 2
+//Numeric literal 5.76; 
+//String Literal: "JavaScript Essentials";
+//Boolean literal: false
+//null literal: null
+
+// STEP 3
+//var givenName = firstName + " " + "Yohannes";
+//var complexExp = givenName * (blnMayWeContactYou + (NumAndStr/5));
+
+// STEP 4
+//var strFirstName;
+//var strLastName;
+//var strAddress;
+//var strCity;
+//var strState;
+//var numZipCode;
+//var numYourAge;
+//var strReferralSource;
+//var blnMayWeContactYou;
+
+// STEP 5
+//var strFirstName;
+//strFirstName = "Lolita";
+//var strLastName;
+//strLastName = "Yohannes";
+//var strAddress;
+//strAddress = "1276 G Street";
+//var strCity = "San Diego";
+//var strState = "CA";
+//var numZipCode = 92104;
+//var numYourAge = 55, strReferralSource = "TV", blnMayWeContactYou = true;
+
+// STEP 6
+//var NumAndStr = 92104 + "2493";
+//var blnMayWeContactYou = true;
+//var numZipCode = 92104;
+//window.console.log(NumAndStr);
+//window.console.log(blnMayWeContactYou + NumAndStr);
+//window.console.log(blnMayWeContactYou + numZipCode);
+
+// STEP 7
+//window.console.log(strStreet);
+//var strStreet = "University";
+
+// STEP 8
+//No, the ' in I'm not should be escaped
+//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//window.console.log(someString);
+
+// STEP 9
+//var myAddress;
+//var nullValue = null;
+//window.console.log(myAddress);
+//window.console.log(" " + nullValue);
+
+// STEP 10
+//var strFirstName = "Lolita";
+//var numYourAge = 55;
+//var blnMayWeContactYou = true;
+//var theNewObject = {};
+//var myAddress;
+//window.console.log(typeof strFirstName);
+//window.console.log(typeof numYourAge);
+//window.console.log(typeof blnMayWeContactYou);
+//window.console.log(typeof theNewObject);
+//window.console.log(typeof myAddress);
+
+// STEP 11
+//window.alert("Hello " + "Lolita Yohannes" + ", welcome to the JavaScript class!")
+
+// STEP 12
+//var name = "Lolita Yohannes";
+//window.alert("Hello " + name + ", welcome to the JavaScript class!")
+
+
+// STEP 13
+//var name = "Lolita Yohannes";
+//var course = "JavaScript";
+//window.alert("Hello " + name + ", welcome to the" + " " + course + " class!")
+
+// STEP 14
+//var name = "Lolita Yohannes";
+//var course = "JavaScript";
+//window.alert("Hello " + name + "\nWelcome to the " + course + "class!")
+
+
+// STEP 15
+//var name = window.prompt("Enter your name:");
+//window.alert("Hello " + name + ", welcome to the JavaScript class!")
+
+// STEP 16
+//var name = window.prompt("Enter your name:");
+//var course = window.prompt("Enter the course you are taking:");
+//window.alert("Hello " + name + ", welcome to the" + " " + course + " class!");
+
+// STEP 17
+//var x = 10;
+//var y = 20;
+//window.console.log(x + y);
+
+// STEP 18
+//var x = 20;
+//x += 20;
+//window.console.log(x);
+
+// STEP 19
+//var x = 20;
+//x *= 5;
+//window.console.log(x);
+
+// STEP 20
+//var x = 20 % 3;
+//x /= 1;
+//window.console.log(x);
+
+// STEP 21
+//var x = 25, y = 56;
+//window.console.log((x * 2) != y);
+
+// STEP 22
+//var x = 26 % 5, y = 5;
+//window.console.log((x / y) >= 5);
+
+// STEP 23
+//var widget = new Object();
+//window.console.log(typeof widget);
+
+// STEP 24
+//var widget = new Object();
+//window.console.log(widget instanceof Object);
+
+// STEP 25
+//var widget = new Object();
+//window.console.log(widget instanceof Boolean);
